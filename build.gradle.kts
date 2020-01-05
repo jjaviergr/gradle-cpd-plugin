1f0b360872ec2054ff14c816b16969d075d73462
Andreas Schmid <service@aaschmid.de>
2020-01-05 18:39:49 +0100

    migrate repositories and new integTest sourcesSet to kotlin-dsl
    Signed-off-by: Andreas Schmid <service@aaschmid.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,4 +19,15 @@ version = "3.2-SNAPSHOT"
 
 val isBuildOnJenkins by extra(System.getenv("BUILD_TAG")?.startsWith("jenkins-") ?: false)
 
+repositories {
+    mavenCentral()
+}
+
+sourceSets {
+    register("integTest") {
+        compileClasspath += main.get().output + test.get().output
+        runtimeClasspath += main.get().output + test.get().output
+    }
+}
+
 apply(from = "legacy-build.gradle")
